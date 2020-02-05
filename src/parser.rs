f19c695b635e81a2d08d0c2ca6bd05405a0da7a7
Richard Dallaway <richard@dallaway.com>
2020-02-05 20:49:39 +0000

    parse individual lines of a journey

--- src/parser.rs
+++ src/parser.rs
@@ -4,28 +4,30 @@ use nom::character::complete::one_of;
 use nom::character::complete::space1;
 use nom::combinator::map_res;
 use nom::sequence::separated_pair;
+use nom::sequence::tuple;
+use nom::multi::many0;
 use nom::*;
 
 use std::num::ParseIntError;
 
 #[derive(Debug, PartialEq)]
-enum Bearing {
+enum Direction {
     N,
     E,
     S,
     W,
 }
 
-struct UnrecognizedBearing;
+struct UnrecognizedDirection;
 
-impl Bearing {
-    fn from(ch: char) -> Result<Bearing, UnrecognizedBearing> {
+impl Direction {
+    fn from(ch: char) -> Result<Direction, UnrecognizedDirection> {
         match ch {
-            'N' => Ok(Bearing::N),
-            'E' => Ok(Bearing::E),
-            'S' => Ok(Bearing::S),
-            'W' => Ok(Bearing::W),
-            _ => Err(UnrecognizedBearing),
+            'N' => Ok(Direction::N),
+            'E' => Ok(Direction::E),
+            'S' => Ok(Direction::S),
+            'W' => Ok(Direction::W),
+            _ => Err(UnrecognizedDirection),
         }
     }
 }
@@ -42,6 +44,12 @@ impl Location {
         let y: u16 = y.parse()?;
         Ok(Location { x, y })
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct RobotState {
+    at: Location,
+    facing: Direction,
 }
 
 #[derive(Debug, PartialEq)]
@@ -64,36 +72,39 @@ impl Movement {
     }
 }
 
-fn location(input: &str) -> IResult<&str, Location> {
-    map_res(separated_pair(digit1, space1, digit1), |(x, y)| {
+fn robot_state(input: &str) -> IResult<&str, RobotState> {
+    // Basic parsing
+    let direction = map_res(one_of("NESW"), Direction::from);
+    let location = map_res(separated_pair(digit1, space1, digit1), |(x, y)| {
         Location::from(x, y)
-    })(input)
+    });
+
+    let parse = tuple((location, space1, direction));
+
+    let (remainder, (at, _, facing)) = parse(input)?;
+    Ok((remainder, RobotState { at, facing }))
 }
 
-fn bearing(input: &str) -> IResult<&str, Bearing> {
-    map_res(one_of("NESW"), Bearing::from)(input)
-}
-
-fn movement(input: &str) -> IResult<&str, Movement> {
-    map_res(one_of("LRF"), Movement::from)(input)
+fn movements(input: &str) -> IResult<&str, Vec<Movement>> {
+    many0( map_res(one_of("LRF"), Movement::from) )(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn test_parse_location() {
-        assert_eq!(location("1 2"), Ok(("", Location { x: 1, y: 2 })));
+    fn test_parse_robot_state() {
+        let at = Location { x: 1, y: 2 };
+        let facing = Direction::N;
+        assert_eq!(robot_state("1 2 N"), Ok(("", RobotState { at, facing })));
     }
 
     #[test]
-    fn test_parse_compass_bearing() {
-        assert_eq!(bearing("E"), Ok(("", Bearing::E)));
+    fn test_parse_movements() {
+        let expected: Vec<Movement> =
+            "FRRFLFFL".chars().flat_map(Movement::from).collect();
+
+        assert_eq!(movements("FRRFLFFL"), Ok(("", expected)));
     }
 
-    #[test]
-    fn test_parse_motion() {
-        assert_eq!(movement("L"), Ok(("", Movement::L)));
-    }
 }
