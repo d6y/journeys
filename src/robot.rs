ed25e2d0bd1f8c76952f520f581eb0ca8e5683f5
Richard Dallaway <richard@dallaway.com>
2020-02-08 20:52:45 +0000

    simplify logic to a single pattern match

--- src/robot.rs
+++ src/robot.rs
@@ -26,76 +26,33 @@ pub enum Direction {
 // 2. With that, we take the starting point and for each move crank the step function to compute each new state.
 //    That's the `fold` function on a collection.
 pub fn run(journey: &Journey) -> RobotState {
-    let step = |state: RobotState, movement| state.step(movement);
+    let step = |state: RobotState, movement: &Movement| {
+        use Direction::{E, N, S, W};
+        use Movement::{F, L, R};
+        match (movement, state.facing, state.at.x, state.at.y) {
+            (F, N, x, y) => RobotState::new(x, y + 1, N),
+            (F, E, x, y) => RobotState::new(x + 1, y, E),
+            (F, S, x, y) => RobotState::new(x, y - 1, S),
+            (F, W, x, y) => RobotState::new(x - 1, y, W),
+            (L, N, x, y) => RobotState::new(x, y, W),
+            (L, E, x, y) => RobotState::new(x, y, N),
+            (L, S, x, y) => RobotState::new(x, y, E),
+            (L, W, x, y) => RobotState::new(x, y, S),
+            (R, N, x, y) => RobotState::new(x, y, E),
+            (R, E, x, y) => RobotState::new(x, y, S),
+            (R, S, x, y) => RobotState::new(x, y, W),
+            (R, W, x, y) => RobotState::new(x, y, N),
+        }
+    };
+
     journey.moves.iter().fold(journey.start.clone(), step)
 }
 
 impl RobotState {
-    #[cfg(test)]
     pub fn new(x: u16, y: u16, facing: Direction) -> RobotState {
         RobotState {
             at: Location { x, y },
             facing,
-        }
-    }
-
-    // Take one step
-    fn step(self, movement: &Movement) -> RobotState {
-        match *movement {
-            Movement::F => RobotState {
-                at: self.at.step(self.facing.clone()),
-                ..self
-            },
-            Movement::L => RobotState {
-                facing: self.facing.left(),
-                ..self
-            },
-            Movement::R => RobotState {
-                facing: self.facing.right(),
-                ..self
-            },
-        }
-    }
-}
-
-impl Direction {
-    fn left(self) -> Direction {
-        match self {
-            Direction::N => Direction::W,
-            Direction::W => Direction::S,
-            Direction::S => Direction::E,
-            Direction::E => Direction::N,
-        }
-    }
-
-    fn right(self) -> Direction {
-        match self {
-            Direction::N => Direction::E,
-            Direction::E => Direction::S,
-            Direction::S => Direction::W,
-            Direction::W => Direction::N,
-        }
-    }
-}
-impl Location {
-    fn step(self, dir: Direction) -> Location {
-        match dir {
-            Direction::N => Location {
-                y: self.y + 1,
-                ..self
-            },
-            Direction::S => Location {
-                y: self.y - 1,
-                ..self
-            },
-            Direction::E => Location {
-                x: self.x + 1,
-                ..self
-            },
-            Direction::W => Location {
-                x: self.x - 1,
-                ..self
-            },
         }
     }
 }
@@ -103,20 +60,6 @@ impl Location {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn one_move_forward() {
-        let start = RobotState::new(0, 0, Direction::N);
-        let end = RobotState::new(0, 1, Direction::N);
-        assert_eq!(start.step(&Movement::F), end);
-    }
-
-    #[test]
-    fn one_turn() {
-        let start = RobotState::new(0, 0, Direction::N);
-        let end = RobotState::new(0, 0, Direction::E);
-        assert_eq!(start.step(&Movement::R), end);
-    }
 
     #[test]
     fn follow_journey_up_and_right() {
